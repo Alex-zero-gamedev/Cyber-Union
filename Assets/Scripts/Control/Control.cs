d459bf75e30f2bdeffbb728d733d921d26ff4aed
Alex-zero-gamedev <w1teblack@yandex.ru>
2021-01-24 21:18:26 +0300

    Добавил камеру и передвижение(Пока в разработке)

---
+++ Assets/Scripts/Control/Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public int speed = 10, shiftw = 10, shifts = 5;
+ 
+    public GameObject player;//здесь ми указываем персонажа как игровой Object;
+   
+    void Start()
+    {
+        player = (GameObject)this.gameObject; //тут присваиваем персонажа к игровому Object или как-то так.
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){ player.transform.position += player.transform.forward * speed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.S)){ player.transform.position -= player.transform.forward * speed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.D)){ player.transform.position += player.transform.right * speed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.A)){ player.transform.position -= player.transform.right * speed * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift)){ player.transform.position += player.transform.forward * (speed + shiftw) * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.LeftShift)){ player.transform.position -= player.transform.forward * (speed + shifts) * Time.deltaTime; }
+       
+    }
+    
+}
