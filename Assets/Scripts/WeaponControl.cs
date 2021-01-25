ca6b98912b54e813ceb0686d981c944f2191781f
Alex-zero-gamedev <w1teblack@yandex.ru>
2021-01-25 21:42:13 +0300

    добавлена физика и стрельба из пистолета(нужно доработать анимацию стрельбы) Угол обзора = 90

--- Assets/Scripts/WeaponControl.cs
+++ Assets/Scripts/WeaponControl.cs
@@ -7,14 +7,14 @@ public class WeaponControl : MonoBehaviour
     // Start is called before the first frame update
     public float damage = 21;
     public float fireRate = 1;
-    public float force = 155;
     public float range = 15;
     public ParticleSystem muzzleFlash;
+    public Transform bulletspawn;
     public AudioClip shotFX;
     public AudioSource _audioSource;
-    public Transform bulletSpawn;
     public Camera _cum;
     public float nextFire = 0f;
+    private bool Shuuting = true;
     void Start()
     {
         
@@ -23,25 +23,24 @@ public class WeaponControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1")) {
-            nextFire = Time.time + 1f / fireRate;
-            Shoot(); 
+        if (Input.GetButtonDown("Fire1")) {
+            //nextFire = Time.time + 1f / fireRate;
+            Shuuting = true;
+            if (Shuuting) Shoot(); 
         }
 
     }
     void Shoot()
     {
         _audioSource.PlayOneShot(shotFX);
-        //Instantiate(muzzleFlash, bulletSpawn.position, bulletSpawn.rotation);
+        //Instantiate(muzzleFlash, bulletspawn.position, bulletspawn.rotation); 
+        muzzleFlash.Play();
         RaycastHit hit;
+
         if (Physics.Raycast(_cum.transform.position, _cum.transform.forward, out hit, range))
         {
-            Debug.Log("AHHHHH i'm Cuming!");
-            muzzleFlash.Play();
-            //GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal))
-            //Destroy(impact, 2f);
-
-            if (hit.rigidbody != null) hit.rigidbody.AddForce(-hit.normal * force);
+            Debug.Log("Cuming!!!");
         }
+        Shuuting = false;
     }
 }
